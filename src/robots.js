c6db0586fa49319b9e1dc26b9669d1b369fb3c76
Alberto <mac.alberto@hotmail.com>
2022-03-30 22:33:55 -0300

    initial commit

---
+++ src/robots.js
@@ -0,0 +1,63 @@
+export const robots = [
+    {
+      id: 5,
+      name: 'Diego Gravisaco',
+      username: 'diego',
+      email: 'diego@april.biz'
+    },
+    {
+      id: 6,
+      name: 'Rony Sousa',
+      username: 'Rony',
+      email: 'rony@melissa.tv'
+    },
+    {
+      id: 7,
+      name: 'Alberto Machuca',
+      username: 'Alberto',
+      email: 'alberto@yesenia.net'
+    },
+    {
+      id: 8,
+      name: 'Patricia Lebsack',
+      username: 'Karianne',
+      email: 'Julianne.OConner@kory.org'
+    },
+    {
+      id: 9,
+      name: 'Chelsey Dietrich',
+      username: 'Kamren',
+      email: 'Lucio_Hettinger@annie.ca'
+    },
+    {
+      id: 10,
+      name: 'Mrs. Dennis Schulist',
+      username: 'Leopoldo_Corkery',
+      email: 'Karley_Dach@jasper.info'
+    },
+    {
+      id: 11,
+      name: 'Kurtis Weissnat',
+      username: 'Elwyn.Skiles',
+      email: 'Telly.Hoeger@billy.biz'
+    },
+    {
+      id: 12,
+      name: 'Nicholas Runolfsdottir V',
+      username: 'Maxime_Nienow',
+      email: 'Sherwood@rosamond.me'
+    },
+    {
+      id: 13,
+      name: 'Glenna Reichert',
+      username: 'Delphine',
+      email: 'Chaim_McDermott@dana.io'
+    },
+    {
+      id: 14,
+      name: 'Clementina DuBuque',
+      username: 'Moriah.Stanton',
+      email: 'Rey.Padberg@karina.biz'
+    }
+  ];
+  
